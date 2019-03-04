210f050254a195504518d4d989c6bd22aacd606d
aengel88 <35244592+aengel88@users.noreply.github.com>
2019-03-04 07:50:54 -0800

    Add files via upload

---
+++ newsdb/precache-manifest.1a9ec2c59c7a96f37178baace7f90710.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "234e35f7b654ad2d4840",
+    "url": "/static/js/main.234e35f7.chunk.js"
+  },
+  {
+    "revision": "8d22494d246f8defe845",
+    "url": "/static/js/1.8d22494d.chunk.js"
+  },
+  {
+    "revision": "234e35f7b654ad2d4840",
+    "url": "/static/css/main.205eb1f9.chunk.css"
+  },
+  {
+    "revision": "730281acd435f4d2a6839e0b899b58bb",
+    "url": "/index.html"
+  }
+];
---
+++ newsdb/precache-manifest.a4ed88b57ce60a1e568ece5461505531.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c6a7c15123e067a761c9",
+    "url": "/static/js/main.c6a7c151.chunk.js"
+  },
+  {
+    "revision": "8d22494d246f8defe845",
+    "url": "/static/js/1.8d22494d.chunk.js"
+  },
+  {
+    "revision": "c6a7c15123e067a761c9",
+    "url": "/static/css/main.205eb1f9.chunk.css"
+  },
+  {
+    "revision": "90b09a8dce79d1180facd9cdeb1b1c25",
+    "url": "/index.html"
+  }
+];
